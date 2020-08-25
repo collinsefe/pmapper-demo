ff035db8690d57386c09f4d1913e9750fd02f644
Joseph Caxton-Idowu <josephcaxtonidowu@yahoo.com>
2020-08-25 22:22:44 +0100

    Add more dependencies

--- principalmapper/util/botocore_tools.py
+++ principalmapper/util/botocore_tools.py
@@ -26,9 +26,9 @@ def get_session(profile_arg: Optional[str]) -> botocore.session.Session:
     Tries to follow order from: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
     """
     # command-line args (--profile)
-    if profile_arg is not None:
-        result = botocore.session.Session(profile=profile_arg)
-    else:  # pull from environment vars / metadata
+    #if profile_arg is not None:
+    #    result = botocore.session.Session(profile=profile_arg)
+    #else:  # pull from environment vars / metadata
         result = botocore.session.get_session()
 
     stsclient = result.create_client('sts')
