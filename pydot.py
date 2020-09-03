241acfa6328d9a6a118de1ae872570d04aedd0e4
Joseph Caxton-Idowu <josephcaxtonidowu@yahoo.com>
2020-09-03 13:35:01 +0100

    bug fix

--- pydot.py
+++ pydot.py
@@ -1815,8 +1815,8 @@ class Dot(Graph):
                 f.write(s)
         else:
             s = self.create(prog, format, encoding=encoding, mypath=path)
-            with io.open(path, mode='wb') as f:
-                f.write(s)
+            #with io.open(path, mode='wb') as f:
+            #    f.write(s)
         return True
 
     def create(self, prog=None, format='ps', encoding=None, mypath=None):
